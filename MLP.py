c828198cdec5f00365417286dcec85625d1a3720
Julio Fuentes <juliojavif@gmail.com>
2021-08-15 13:02:21 -0500

    first commit

---
+++ MLP.py
@@ -0,0 +1,59 @@
+
+class MLP_regressor:
+
+    """Import the required modules"""
+    from sklearn.neural_network import MLPRegressor
+    from sklearn.model_selection import train_test_split
+    from sklearn.datasets import fetch_california_housing
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.metrics import r2_score
+    import pandas as pd
+
+    def __init__(self) -> None:
+        pass
+
+    cal_housing = fetch_california_housing()
+    X = pd.DataFrame(cal_housing.data, columns=cal_housing.feature_names)
+    y = cal_housing.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.2)
+
+    sc_X = StandardScaler()
+    X_trainscaled = sc_X.fit_transform(X_train)
+    X_testscaled = sc_X.transform(X_test)
+
+    reg = MLPRegressor(hidden_layer_sizes=(64,64,64), activation="relu", random_state=1, max_iter=2000).fit(X_trainscaled, y_train)
+
+    y_pred = reg.predict(X_testscaled)
+    print("The Score whit", (r2_score(y_pred, y_test)))
+
+
+class MLP_classifier:
+
+    def __init__(self) -> None:
+        pass
+
+    """Import the required modules"""
+    from sklearn.datasets import load_iris
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import StandardScaler
+    import pandas as pd
+    from sklearn.metrics import plot_confusion_matrix
+    import matplotlib.pyplot as plt
+
+    iris_data = load_iris()
+    X = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+    y = iris_data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=1,test_size=0.2)
+    sc_X = StandardScaler()
+    X_trainscaled = sc_X.fit_transform(X_train)
+    X_testscaled = sc_X.transform(X_test)
+
+    clf = MLPClassifier(hidden_layer_sizes=(256,128,64,32),activation="relu",random_state=1).fit(X_trainscaled, y_train)
+    y_pred = clf.predict(X_testscaled)
+    print("Result: ", clf.score(X_testscaled, y_test))
+
+    fig = plot_confusion_matrix(clf, X_testscaled, y_test, display_labels=["Setosa", "Versicolor", "Virginica"])
+    fig.figure_.suptitle("Confusion Matrix for Iris Dataset")
+    plt.show()
---
+++ main.py
@@ -0,0 +1,8 @@
+from MLP import MLP_classifier, MLP_regressor
+
+def main():
+    mlp = MLP_regressor()
+    mlp = MLP_classifier()
+
+if __name__ == '__main__':
+    main()
